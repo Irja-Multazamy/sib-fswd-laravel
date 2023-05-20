dcced0600746c36fc1b189bef39094cab0f33fa9
Irja-Multazamy <irjamultazami@gmail.com>
2023-05-20 16:10:17 +0700

    menambah project laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('user.index');
+    }
+    public function tambah()
+    {
+        return view('user.tambah');
+    }
+    public function edit()
+    {
+        return view('user.edit');
+    }
+}
