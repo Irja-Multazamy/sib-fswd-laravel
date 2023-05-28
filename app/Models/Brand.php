63385c99d051694de0aafed7426421782aa68c9c
Irja-Multazamy <irjamultazami@gmail.com>
2023-05-28 19:11:59 +0700

    edit dan menambahkan tugas project laravel CRUD

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,8 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Users extends Model
+class Brand extends Model
 {
     use HasFactory;
-    protected $table = 'users';
+    protected $table = "brands";
+    protected $fillable = [
+        'name',
+    ];
 }
